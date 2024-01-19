6980172c9c3784d94339fc31f48e473629810746
BitcoinWukong <BitcoinWukong@proton.me>
2024-01-19 00:17:12 +0000

    Several improments
    - Add exception handling
    - Auto retry when make order or cancel order fails
    - Use get_rebot_info to always get most up-to-date active_order_id
    - Make start_hour and end_hour parameters

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # robosats-maker
 A python script to simplify the life of robosats maker.
 
-The script creates a new order at 8AM UTC, and cancel the order at 11PM UTC. You can adjust the value at line 211 and line 236 of the python script.
+By default, the script creates a new order at 8AM UTC, and cancel the order at 11PM UTC. You can adjust the value by setting `--start_hour` and `--end_hour` parameters.
 
 The script relies on blink.sv wallet API to pay for the maker bond. In the future, we may consider support other LN providers, but only Blink wallet is supported at this moment.
 
@@ -17,16 +17,17 @@ The script relies on blink.sv wallet API to pay for the maker bond. In the futur
 `pip3 install -r requirements.txt`
 1. Run the script:
 `robosats-maker.py [--type {0,1}] [--currency {1,2,3,4,5,6}] robosats_token blink_api_key amount payment_method premium`
+
 For example:
-`python3 robosats-maker.py --type 1 --currency 2 $robosats_token $blink_api_token 100 "USDT" 5`
+`python3 robosats-maker.py --start_hour 15 --end_hour 23 --type 1 --currency 2 $$robosats_token $$blink_api_token 100 "USDT" 20`
 
 # Notes
 
-
 For more details, you can check `python3 robosats-maker.py -h`:
 ```
-python3 robosats-maker.py -h
-usage: robosats-maker.py [-h] [--type {0,1}] [--currency {1,2,3,4,5,6}] robosats_token blink_api_key amount payment_method premium
+➜  python3 robosats-maker.py -h
+usage: robosats-maker.py [-h] [--type {0,1}] [--currency {1,2,3,4,5,6}] [--start_hour START_HOUR] [--end_hour END_HOUR]
+                         robosats_token blink_api_key amount payment_method premium
 
 RoboSats Maker Script
 
@@ -34,12 +35,18 @@ positional arguments:
   robosats_token        RoboSats token
   blink_api_key         Blink API key
   amount                Amount for the order
-  payment_method        Payment Method
+  payment_method        Payment Method, more details:
+                        https://github.com/RoboSats/robosats/blob/main/frontend/src/components/PaymentMethods/MethodList.ts
   premium               Premium for the order
 
-options:
+optional arguments:
   -h, --help            show this help message and exit
   --type {0,1}          Order type: 0 for buy, 1 for sell (default: 0)
   --currency {1,2,3,4,5,6}
-                        Currency: 1 for USD, 2 for EUR, etc. (default: 2), more details: https://github.com/RoboSats/robosats/blob/main/frontend/static/assets/currencies.json
+                        Currency: 1 for USD, 2 for EUR, etc. (default: 2), more details:
+                        https://github.com/RoboSats/robosats/blob/main/frontend/static/assets/currencies.json
+  --start_hour START_HOUR
+                        Start hour in UTC
+  --end_hour END_HOUR   End hour in UTC
+
 ```
--- robosats-maker.py
+++ robosats-maker.py
@@ -105,6 +105,9 @@ async def make_general_request(api, token, query_params, form_body_params, metho
 async def get_order_details(token, order_id):
     return await make_general_request("order", token, {"order_id": str(order_id)}, {}, "GET")
 
+async def get_robot_info(token):
+    return await make_general_request("robot", token, {}, {}, "GET")
+
 async def cancel_order(token, order_id):
     return await perform_order_action(token, order_id, "cancel")
 
@@ -159,13 +162,15 @@ async def get_wallet_info(auth_token):
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='RoboSats Maker Script')
-    parser.add_argument('robosats_token', help='RoboSats token')
-    parser.add_argument('blink_api_key', help='Blink API key')
+    parser.add_argument('robosats_token', type=str, help='RoboSats token')
+    parser.add_argument('blink_api_key', type=str, help='Blink API key')
     parser.add_argument('amount', type=float, help='Amount for the order')
-    parser.add_argument('payment_method', type=str, help='Payment Method')
+    parser.add_argument('payment_method', type=str, help='Payment Method, more details: https://github.com/RoboSats/robosats/blob/main/frontend/src/components/PaymentMethods/MethodList.ts')
     parser.add_argument('premium', type=float, help='Premium for the order')
     parser.add_argument('--type', type=int, default=0, choices=[0, 1], help='Order type: 0 for buy, 1 for sell (default: 0)')
     parser.add_argument('--currency', type=int, default=2, choices=range(1, 7), help='Currency: 1 for USD, 2 for EUR, etc. (default: 2), more details: https://github.com/RoboSats/robosats/blob/main/frontend/static/assets/currencies.json')
+    parser.add_argument('--start_hour', type=int, default=8, help='Start hour in UTC')
+    parser.add_argument('--end_hour', type=int, default=23, help='End hour in UTC')
     return parser.parse_args()
 
 async def main():
@@ -176,14 +181,13 @@ async def main():
     amount = args.amount
     premium = args.premium
     payment_method = args.payment_method
+    start_hour = args.start_hour
+    end_hour = args.end_hour
 
     # 0: buy, 1: sell
     order_type = args.type
     # 1: usd, 2: eur, 3: jpy, 4: gbp, 5: aud, 6: cad (https://github.com/RoboSats/robosats/blob/main/frontend/static/assets/currencies.json)
     currency = args.currency
-
-    get_info_result = await get_info(robosats_token)
-    print(f"Info Result: {get_info_result}")
 
     # Get wallet information
     wallet_info = await get_wallet_info(blink_api_key)
@@ -200,48 +204,79 @@ async def main():
 
     print("BTC Wallet ID:", btc_wallet_id)
 
-    order_id = None
-
     while True:
         utc_time = datetime.now(pytz.utc)
         hour = utc_time.hour
+        if (utc_time.minute % 30 == 0):
+            print(f'[{utc_time}] hour: {hour}')
 
-        print(f'hour: {hour}')
+        try:
+            # Always get active order id by checking robot info
+            robot_info = await get_robot_info(robosats_token)
+            order_id = robot_info.get('active_order_id', None)
+        except Exception as e:
+            print(f'Error getting robot info: {e}')
+            continue
 
-        if hour == 8 and order_id is None:
-            print(f"Making order at {utc_time}")
-            order_result = await make_order(
-                token=robosats_token,
-                type=order_type,
-                currency=currency,
-                amount=amount,
-                payment_method=payment_method,
-                premium=premium,
-                public_duration=15 * 60 * 60,
-                escrow_duration=3 * 60 * 60,
-                bond_size="3.0"
-            )
-            print("Order Result:", order_result)
-            order_id = order_result['id']
+        # For example: start at 10:00, ends at 17:00
+        if (start_hour < end_hour and (hour >= start_hour and hour < end_hour)) \
+            or (start_hour > end_hour and (hour >= start_hour or hour < end_hour)): # For example: start at 22:00, ends at 3:00
+            if order_id is None:
+                try:
+                    # Make order if there is no active order
+                    print(f"Making order at {utc_time}")
+                    order_result = await make_order(
+                        token=robosats_token,
+                        type=order_type,
+                        currency=currency,
+                        amount=amount,
+                        payment_method=payment_method,
+                        premium=premium,
+                        public_duration=24 * 60 * 60,
+                        escrow_duration=3 * 60 * 60,
+                        bond_size="3.0"
+                    )
+                    print("Order Result:", order_result)
+                    order_id = order_result['id']
 
-            order_details = await get_order_details(robosats_token, order_id)
-            print("Order Details:", order_details)
+                    retry_count = 0
+                    max_retries = 30
+                    while retry_count < max_retries:
+                        try:
+                            order_details = await get_order_details(robosats_token, order_id)
+                            print("Order Details:", order_details)
 
-            bond_invoice = order_details['bond_invoice']
-            print("Bond Invoice:", bond_invoice)
+                            bond_invoice = order_details['bond_invoice']
+                            print("Bond Invoice:", bond_invoice)
 
-            payment_result = await pay_bond_invoice(blink_api_key, bond_invoice, btc_wallet_id)
-            print("Payment Result:", payment_result)
-
-        elif hour == 23 and order_id is not None:
-            print(f"Cancelling order at {utc_time}")
+                            payment_result = await pay_bond_invoice(blink_api_key, bond_invoice, btc_wallet_id)
+                            print("Payment Result:", payment_result)
+                            break
+                        except Exception as e:
+                            print(f'Paying bond invoice failed: {e}')
+                            retry_count += 1
+                        await asyncio.sleep(30)
+                    if retry_count == max_retries:
+                        # Reset order_id if unable to pay for it.
+                        order_id = None
+                except Exception:
+                    print("Make order failed")
+        elif order_id is not None:
             try:
                 cancel_result = await cancel_order(robosats_token, order_id)
                 print(f"Cancel Order Result: {cancel_result}")
-            except HTTPError as http_err:
+            except HTTPError:
                 # Cancel order is expected to fail
-                print('Cancel Order may have succeeded')
-            order_id = None  # Reset order ID after cancellation
+                pass
+
+            try:
+                robot_info = await get_robot_info(robosats_token)
+                print(f'Robot Info: {robot_info}')
+                order_id = robot_info.get('active_order_id', None)
+                if order_id is None:
+                    print('Cancel Order succeeded')
+            except Exception as e:
+                print(f'Error getting robot info: {e}')
 
         # Wait for 60 seconds before next check
         await asyncio.sleep(60)
